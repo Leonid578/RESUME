2a903aaf659a7fb6d432bf34f2c3c045fcb9cb75
Leonid578 <Leonid.Ivanov187@gmail.com>
2023-01-11 19:54:36 +0200

    upd 0.1.1

---
+++ my-app/src/Description/Description.jsx
@@ -0,0 +1,17 @@
+import "./Description.styled.css";
+
+const Description = () => {
+  return (
+    <div className="description">
+      <h2 className="titleText">Hi!!!</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates unde
+        quos non vitae esse maiores dignissimos voluptatibus alias quaerat
+        consequuntur fugit distinctio accusamus, quis dolor soluta blanditiis,
+        adipisci quidem minus.
+      </p>
+    </div>
+  );
+};
+
+export default Description;
---
+++ my-app/src/RunningTitle/RunningTitle.jsx
@@ -0,0 +1,11 @@
+import "./RunningTitle.style.css";
+
+const RunningTitle = () => {
+  return (
+    <div className="runningTitle">
+      <h1 className="titleText">Hi</h1>
+    </div>
+  );
+};
+
+export default RunningTitle;
