1c12de10677ed1ab0303613dcefd27cbe154da41
Leonid578 <Leonid.Ivanov187@gmail.com>
2023-01-13 20:44:14 +0200

    upd

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -3,8 +3,30 @@ import "./Description.styled.css";
 const Description = () => {
   return (
     <div className="description">
-      <h2 className="titleText">Hi!!!</h2>
-      <p>
+      <h1 className="titleTextDesc">Leonid Ivanov</h1>
+      <h2 className="titleTextDesc">Front-end developer</h2>
+      <p className="text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates unde
+        quos non vitae esse maiores dignissimos voluptatibus alias quaerat
+        consequuntur fugit distinctio accusamus, quis dolor soluta blanditiis,
+        adipisci quidem minus.
+      </p>
+      <h2 className="titleTextDesc">Front-end developer</h2>
+      <p className="text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates unde
+        quos non vitae esse maiores dignissimos voluptatibus alias quaerat
+        consequuntur fugit distinctio accusamus, quis dolor soluta blanditiis,
+        adipisci quidem minus.
+      </p>
+      <h2 className="titleTextDesc">Front-end developer</h2>
+      <p className="text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates unde
+        quos non vitae esse maiores dignissimos voluptatibus alias quaerat
+        consequuntur fugit distinctio accusamus, quis dolor soluta blanditiis,
+        adipisci quidem minus.
+      </p>
+      <h2 className="titleTextDesc">Front-end developer</h2>
+      <p className="text">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates unde
         quos non vitae esse maiores dignissimos voluptatibus alias quaerat
         consequuntur fugit distinctio accusamus, quis dolor soluta blanditiis,
--- src/components/RunningTitle/RunningTitle.jsx
+++ src/components/RunningTitle/RunningTitle.jsx
@@ -3,7 +3,48 @@ import "./RunningTitle.style.css";
 const RunningTitle = () => {
   return (
     <div className="runningTitle">
-      <h1 className="titleText">Hi</h1>
+      <div className="fhoto"></div>
+      <h2 className="titleText">Contact information</h2>
+      <div className="pass"></div>
+      <ul>
+        <li className="text">
+          <h3>Phone</h3>
+          <p>+380686082451</p>
+        </li>
+        <li className="text">
+          <h3>Email</h3>
+          <p>Leonid.Ivanov187@gmail.com</p>
+        </li>
+        <li className="text">
+          <h3>LinkedIn</h3>
+          <p>https://www.linkedin.com/in/leonid-ivanov-108a53257/</p>
+        </li>
+        <li className="text">
+          <h3>WWW</h3>
+          <p>https://github.com/Leonid578</p>
+        </li>
+      </ul>
+      <h2 className="titleText">Back-End</h2>
+      <h2 className="titleText">Contact information</h2>
+      <div className="pass"></div>
+      <ul>
+        <li className="text">
+          <h3>NodeJS</h3>
+          <p>FrameWork: Express, WebSoket</p>
+        </li>
+        <li className="text">
+          <h3></h3>
+          <p></p>
+        </li>
+        <li className="text">
+          <h3></h3>
+          <p></p>
+        </li>
+        <li className="text">
+          <h3></h3>
+          <p></p>
+        </li>
+      </ul>
     </div>
   );
 };
